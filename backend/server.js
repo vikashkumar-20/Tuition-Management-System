8a1a352116df111158e5b3df1f6184042ef0c91f
vikashkumar-20 <skvikash7667@gmail.com>
2025-09-04 18:59:58 +0530

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -50,27 +50,26 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 /**
- * ================== 2. CSP Middleware (optional) ==================
- * Tip: Disable this if you face frontend fetch/XHR blocking issues
+ * ================== 2. CSP Middleware ==================
  */
 if (process.env.ENABLE_CSP === "true") {
   app.use((req, res, next) => {
     res.setHeader(
-  "Content-Security-Policy",
-  "default-src 'self'; " +
-  "connect-src 'self' http://localhost:5000 https://api.razorpay.com https://checkout.razorpay.com https://lumberjack.razorpay.com https://www.gstatic.com https://www.googleapis.com https://identitytoolkit.googleapis.com https://securetoken.googleapis.com https://firestore.googleapis.com https://ck-study-backend.vercel.app https://ckstudyclasses.s3.eu-north-1.amazonaws.com; " +
-  "script-src 'self' https://checkout.razorpay.com; " +
-  "script-src-elem https://checkout.razorpay.com; " +
-  "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://cdnjs.cloudflare.com; " +
-  "font-src 'self' https://fonts.gstatic.com https://cdnjs.cloudflare.com; " +
-  "frame-src 'self' https://checkout.razorpay.com https://api.razorpay.com https://www.youtube.com https://*.firebaseapp.com https://apis.google.com; " +
-  "img-src 'self' data: blob: https://*.s3.eu-north-1.amazonaws.com https://*.vercel.app https://*.onrender.com; " +
-  "report-uri /csp-violation-report-endpoint;"
-);
+      "Content-Security-Policy",
+      [
+        "default-src 'self'",
+        "connect-src 'self' http://localhost:5000 https://api.razorpay.com https://checkout.razorpay.com https://lumberjack.razorpay.com https://www.gstatic.com https://www.googleapis.com https://identitytoolkit.googleapis.com https://securetoken.googleapis.com https://firestore.googleapis.com https://ck-study-backend.vercel.app https://ckstudyclasses.s3.eu-north-1.amazonaws.com",
+        "script-src 'self' https://checkout.razorpay.com",
+        "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://cdnjs.cloudflare.com",
+        "font-src 'self' https://fonts.gstatic.com https://cdnjs.cloudflare.com",
+        "frame-src 'self' https://checkout.razorpay.com https://api.razorpay.com https://www.youtube.com https://*.firebaseapp.com https://apis.google.com",
+        "img-src 'self' data: blob: https://*.s3.eu-north-1.amazonaws.com https://*.vercel.app https://*.onrender.com",
+        "report-uri /csp-violation-report-endpoint",
+      ].join("; ")
+    );
     next();
   });
 }
-
 
 /**
  * ================== 3. Routes ==================
